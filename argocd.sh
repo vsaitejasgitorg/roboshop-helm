b50776291e02ebc0b53f82917d38b111e6b8d55f
vadlasaiteja-git <saitejav236@gmail.com>
2025-04-05 17:21:11 +0530

    Working with ArgoCD

--- argocd.sh
+++ argocd.sh
@@ -6,4 +6,4 @@ component_name=$1
 env=$2
 imageTag=$3
 
-argocd app create ${component_name} --repo https://github.com/vsaitejasgitorg/roboshop-helm.git --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
+argocd app create ${component_name}  --upsert --repo https://github.com/vsaitejasgitorg/roboshop-helm.git --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
