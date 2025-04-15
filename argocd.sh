d3b936091d1fc314b1f697994bb2085723b72885
vadlasaiteja-git <saitejav236@gmail.com>
2025-04-15 20:14:40 +0530

    Reconfigured argocd.sh for login error

--- argocd.sh
+++ argocd.sh
@@ -1,10 +1,11 @@
-URL=$(kubectl get svc argocd-server -n argocd | grep argocd| awk '{print $4}')
-PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password |xargs| base64 --decode)
-argocd login $URL --grpc-web --insecure --username admin --password $PASSWORD
-
+#URL=$(kubectl get svc argocd-server -n argocd | grep argocd| awk '{print $4}')
 component_name=$1
 env=$2
 imageTag=$3
+
+PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password |xargs| base64 --decode)
+argocd login argocd-${env}.saitejasroboshop.store --grpc-web --insecure --username admin --password $PASSWORD
+
 
 argocd app create ${component_name}  --upsert --repo https://github.com/vsaitejasgitorg/roboshop-helm.git --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
 
